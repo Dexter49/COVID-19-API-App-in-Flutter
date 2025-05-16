374f4bc4958bebc484e34ab4d370432c670680e9
Daniel Tobi <dexterousguru49@yahoo.com>
2025-05-16 21:01:30 +0100

    Add files via upload
    I've added functionality for Web, Linux, MacOS, and Windows

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Dexter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Dexter\\flutter-apps\\COVID_App" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Dexter\\flutter"
+  "PROJECT_DIR=C:\\Users\\Dexter\\flutter-apps\\COVID_App"
+  "FLUTTER_ROOT=C:\\Users\\Dexter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Dexter\\flutter-apps\\COVID_App\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Dexter\\flutter-apps\\COVID_App"
+  "FLUTTER_TARGET=C:\\Users\\Dexter\\flutter-apps\\COVID_App\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Dexter\\flutter-apps\\COVID_App\\.dart_tool\\package_config.json"
+)
